6372a2df51c9bbe4a056488c9a339e0ac4905c2a
Javier <javiermillervelasco@gmail.com>
2016-05-01 02:53:09 +0200

    New project

---
+++ android/src/com/wiseland/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.wiseland.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.wiseland.game.WiseLand;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WiseLand(), config);
+	}
+}
